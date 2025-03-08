7564d42ff56ed60d2a5d76bc5112608f52c0710d
TribalBeast999 <hifidevil999@gmail.com>
2025-03-08 21:02:52 +0530

    Add example of fetching user data from GitHub API using promises

--- 09_advanced_one/promises.js
+++ 09_advanced_one/promises.js
@@ -79,3 +79,24 @@ async function consumePromiseFive() {
 }
 
 consumePromiseFive();
+
+// async function getAllUsers() {
+//   try{
+//     const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//     const data = await response.json()
+//     console.log(data);
+//   }catch(error){
+//     console.log("E: ",error)
+//   }
+// }
+
+//getAllUsers()
+
+fetch("https://api.github.com/users/akatarunchauhan")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => console.log(error));
