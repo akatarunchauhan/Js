f680643c82a12b9a5d1e2b5ab7a14807d6e36549
TribalBeast999 <hifidevil999@gmail.com>
2025-03-12 00:56:25 +0530

    Add examples of JavaScript array types and performance considerations

---
+++ 11_fun_js/test.js
@@ -0,0 +1,53 @@
+const myArr = []; //% DebugPrint(myArr);
+
+// Continous, Holey
+
+// SMI (small integer)
+// Packed element
+// Double (float, string, function)
+
+const arrTwo = [1, 2, 3, 4, 5];
+// PACKED_SMI_ELEMENTS
+
+arrTwo.push(6.0);
+// PACKED_DOUBLE_ELEMENTS //* Once the array is downgraded, it can't be upgraded even if we delete the elements
+
+arrTwo.push("7");
+// PACKED_ELEMENTS
+
+arrTwo[10] = 11;
+// HOLEY_ELEMENTS
+
+console.log(arrTwo);
+console.log(arrTwo.length);
+console.log(arrTwo[9]);
+
+// bound check
+// hasOwnProperty(arrTwo, 9)
+// hasOwnProperty(arrTwo.prototype, 10)
+
+//* holes are very expensive in JS
+
+const arrThree = [1, 2, 3, 4, 5];
+console.log(arrThree[8]);
+
+// SMI > DOUBLE > PACKED
+// H_SMI > H_DOUBLE > H_PACKED
+
+const arrFour = new Array(3);
+// just 3 holes. HOLEY_SMI_ELEMENTS
+arrFour[0] = "1"; //HOLEY_ELEMENTS
+arrFour[1] = "2"; //HOLEY_ELEMENTS
+arrFour[2] = "3"; //HOLEY_ELEMENTS
+
+const arrFive = []; //* better approach than arrFour[]
+arrFive.push("1"); //PACKED_ELEMENTS
+arrFive.push("2"); //PACKED_ELEMENTS
+arrFive.push("3"); //PACKED_ELEMENTS
+
+const arrSix = [1, 2, 3, 4, 5];
+
+arrSix.push(NaN); //DOUBLE
+arrSix.push(Infinity); //DOUBLE
+
+// for, for-of, forEach }=> use of these is prefered for greater optimisation
